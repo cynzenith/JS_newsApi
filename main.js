60e9fb116715ddd40d5bed5a085c7fda16f5130f
cynzenith~ <cynzenith@gmail.com>
2024-07-12 06:34:31 +0900

    Part10

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const API_KEY = `be38e0d410064dd3b43d39e58e44aeb4`
 let newsList = []
 
+const getLatestNews = async () => {
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
+    console.log(url);
+    const response = await fetch(url)
+    const data = await response.json();
+    newsList = data.articles
+    render()
+    console.log(newsList);
+}
+
 //9-1. 버튼들에 클릭이벤트 주기
 const menus = document.querySelectorAll(".menus button")
 // 배열 형태 출력
@@ -8,17 +18,10 @@ const menus = document.querySelectorAll(".menus button")
 // menus(배열)을 반복 -> menu라는 매개변수를 만들어서 각 요소들이 반복되면서 클릭이벤트를 발생시키고 -> 그 이벤트가 발생시 getNewsByCategory함수를 호출한다.
 menus.forEach(menu=>menu.addEventListener("click",(event)=>getNewsByCategory(event)))
 
-const getLatestNews = async () => {
-        // const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`; 
-        // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
-        const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
-        console.log(url);
-        const response = await fetch(url)
-        const data = await response.json();
-        newsList = data.articles
-        render()
-        console.log(newsList);
-}
+//9-4. 사이드버튼들에 클릭이벤트 주기
+const side_menus = document.querySelectorAll(".sidenav a");
+// console.log(side_menus);
+side_menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)));
 
 const getNewsByCategory = async (event) => {
     // 호출 테스트
@@ -26,6 +29,7 @@ const getNewsByCategory = async (event) => {
 
     //9-2. 카테고리별 뉴스 가져오기
     const category = event.target.textContent.toLowerCase() // 중요
+
     // 호출 테스트
     console.log(category)
     const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`);
@@ -37,6 +41,32 @@ const getNewsByCategory = async (event) => {
     newsList = data.articles;
     render();
 }
+
+// 10-1. 키워드별 검색 - 검색이벤트
+const getNewsByKeyword= async() => {
+    // 테스트
+    // console.log("getNewsByKeyword");
+
+    const searchInput = document.getElementById("search-input");
+    const keyword = searchInput.value;
+    // 테스트
+    // console.log(keyword);
+
+    // 10-2. 키워드별 뉴스 가져오기
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`);
+    const response = await fetch(url)  // async - await
+    const data = await response.json()
+    // 테스트
+    // console.log("keyword", data);
+
+    //10-3. 그 뉴스를 보여주기
+    newsList = data.articles;
+    render();
+
+    // 키워드 입력창 초기화
+    searchInput.value = "";
+}
+
 
 const render=()=>{
     const maxLength = 200;
@@ -76,8 +106,19 @@ const openSearchBox = () => {
     }
 };
 
+// 10-1-1. 입력창에서 enter키 입력
+function handleKeyDown(event) {
+    if (event.key === "Enter") {
+        getNewsByKeyword();
+    }
+}
+
 
 //9-1. 버튼들에 클릭이벤트 주기
 //9-2. 카테고리별 뉴스 가져오기
 //9-3. 그 뉴스를 보여주기
+//9-4. 사이드버튼들에 클릭이벤트 주기
 
+//10-1. 키워드별 검색 - 검색이벤트
+//10-2. 키워드별 뉴스 가져오기
+//10-3. 그 뉴스를 보여주기
